f4c0f64fb450e8c47288e2d4f615b38b9746d1b5
DomTripodi93 <dtripodi93@gmail.com>
2020-04-12 07:58:32 -0400

    added user reducer and base redux store, mapped user reducer values to app component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,26 +1,41 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import { connect } from 'react-redux';
 
-function App() {
+import './App.scss';
+import { checkUser } from './reducers/user/user.actions';
+
+
+const App = (props) => {
+  const [authValue, setAuthValue] = useState(props.isAuthenticated);
+
+  useEffect(() => {
+    let token = localStorage.getItem('token');
+    let userId = localStorage.getItem('id');
+    if (!props.isAuthenticated) {
+      props.checkUser(userId, token);
+    }
+    setAuthValue(props.isAuthenticated)
+  }, [props]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="page">
+      <div>
+        <h1>
+          Welcome to My Budget!
+        </h1>
+      </div>
     </div>
   );
+};
+
+const mapDispatchToProps = dispatch => {
+  return {
+    checkUser: (userId, token) => dispatch(checkUser(userId, token))
+  }
 }
 
-export default App;
+const mapStateToProps = state => ({
+  isAuthenticated: state.user.isAuthenticated
+});
+
+export default connect(mapStateToProps, mapDispatchToProps)(App);
