40cd1a78c2b6528bc56e5b2afb7fdce6f85e5766
DomTripodi93 <dtripodi93@gmail.com>
2020-04-12 08:04:18 -0400

    added styled button and input, and user registration, login and logout components and routes

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,41 +1,56 @@
 import React, { useState, useEffect } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { connect } from 'react-redux';
 
 import './App.scss';
 import { checkUser } from './reducers/user/user.actions';
 
+import Register from './containers/registration/registration';
+import Signin from './containers/registration/signin';
+import Signout from './containers/registration/signout';
+
 
 const App = (props) => {
-  const [authValue, setAuthValue] = useState(props.isAuthenticated);
+    const [authValue, setAuthValue] = useState(props.isAuthenticated);
 
-  useEffect(() => {
-    let token = localStorage.getItem('token');
-    let userId = localStorage.getItem('id');
-    if (!props.isAuthenticated) {
-      props.checkUser(userId, token);
-    }
-    setAuthValue(props.isAuthenticated)
-  }, [props]);
+    useEffect(() => {
+        let token = localStorage.getItem('token');
+        let userId = localStorage.getItem('id');
+        if (!props.isAuthenticated) {
+            props.checkUser(userId, token);
+        }
+        setAuthValue(props.isAuthenticated)
+    }, [props]);
 
-  return (
-    <div id="page">
-      <div>
-        <h1>
-          Welcome to My Budget!
+    return (
+        <div id="page">
+            <div>
+                <h1>
+                    Welcome to My Budget!
         </h1>
-      </div>
-    </div>
-  );
+                {authValue ?
+                    <Switch>
+                        <Route exact path='/signout' component={Signout} />
+                    </Switch>
+                    :
+                    <Switch>
+                        <Route exact path='/register' component={Register} />
+                        <Route exact path='/signin' component={Signin} />
+                    </Switch>
+                }
+            </div>
+        </div>
+    );
 };
 
 const mapDispatchToProps = dispatch => {
-  return {
-    checkUser: (userId, token) => dispatch(checkUser(userId, token))
-  }
+    return {
+        checkUser: (userId, token) => dispatch(checkUser(userId, token))
+    }
 }
 
 const mapStateToProps = state => ({
-  isAuthenticated: state.user.isAuthenticated
+    isAuthenticated: state.user.isAuthenticated
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);
