bd3ba4f73aeecfd4aafe6160a5f86f712106747e
DomTripodi93 <dtripodi93@gmail.com>
2020-04-26 18:04:17 -0400

    added recurring transaction reducer

---
+++ frontend/src/reducers/recurring-transaction/recurring-transaction.reducer.js
@@ -0,0 +1,80 @@
+import RecurringTransactionActionTypes from './recurringTransaction.types';
+
+const INITIAL_STATE = {
+    recurringTransactions: [],
+    selectedRecurringTransaction: {},
+    called: false
+}
+
+const recurringTransactionReducer = (state = INITIAL_STATE, action) => {
+    var sortRecurringTransactions = (recurringTransactions) => {
+        return recurringTransactions.sort((first, second) => {
+            if (first.date < second.date) {
+                return 1;
+            } else {
+                return -1;
+            }
+        });
+    }
+    let recurringTransactionsHold = [ ...state.recurringTransactions ]
+    switch (action.type) {
+        case RecurringTransactionActionTypes.SET_SINGLE_RECURRING_TRANSACTION:
+            return {
+                ...state,
+                selectedRecurringTransaction: action.payload
+            };
+        case RecurringTransactionActionTypes.SET_RECURRING_TRANSACTIONS:
+            if (action.payload.data.length > 0) {
+                recurringTransactionsHold = action.payload.data;
+            } else {
+                recurringTransactionsHold = [];
+            }
+            return {
+                ...state,
+                recurringTransactions: recurringTransactionsHold,
+                called: true
+            };
+        case RecurringTransactionActionTypes.ADD_RECURRING_TRANSACTION:
+            recurringTransactionsHold = sortRecurringTransactions([
+                action.payload,
+                ...recurringTransactionsHold
+            ]);
+            return {
+                ...state,
+                recurringTransactions: recurringTransactionsHold
+            };
+        case RecurringTransactionActionTypes.UPDATE_RECURRING_TRANSACTIONS:
+            recurringTransactionsHold = sortRecurringTransactions([
+                action.payload,
+                ...recurringTransactionsHold
+                    .filter((value) => {
+                        return value.id !== action.payload.id;
+                    })
+            ]);
+            return {
+                ...state,
+                recurringTransactions: recurringTransactionsHold
+            };
+        case RecurringTransactionActionTypes.DELETE_RECURRING_TRANSACTION:
+            recurringTransactionsHold = [
+                ...recurringTransactionsHold
+                    .filter((value) => {
+                        return value.id !== action.payload;
+                    })
+            ]
+            return {
+                ...state,
+                recurringTransactions: recurringTransactionsHold
+            };
+        case RecurringTransactionActionTypes.SIGNOUT_USER:
+            return {
+                recurringTransactions: {},
+                selectedRecurringTransaction: {},
+                called: {}
+            };
+        default:
+            return state;
+    }
+}
+
+export default recurringTransactionReducer;
