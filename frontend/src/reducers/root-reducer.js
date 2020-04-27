34f4f6b82553a53573d63904cceb00abd07a06cc
DomTripodi93 <dtripodi93@gmail.com>
2020-04-27 16:08:55 -0400

    hooked reducers into root reducer

--- frontend/src/reducers/root-reducer.js
+++ frontend/src/reducers/root-reducer.js
@@ -1,8 +1,14 @@
 import { combineReducers } from 'redux';
 
 import userReducer from './user/user.reducer';
+import accountReducer from './account/account.reducer';
+import transactionReducer from './transaction/transaction.reducer';
+import recurringTransactionReducer from './recurring-transaction/recurring-transaction.reducer';
 
 
 export default combineReducers({
-    user: userReducer
+    user: userReducer,
+    account: accountReducer,
+    transaction: transactionReducer,
+    recurringTransaction: recurringTransactionReducer
 })
