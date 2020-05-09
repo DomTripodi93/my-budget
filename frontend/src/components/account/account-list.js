00c7c25a2e5894b8a01284a21a8b7b55266a600b
DomTripodi93 <dtripodi93@gmail.com>
2020-05-09 08:55:21 -0400

    configured base styling for account display

--- frontend/src/components/account/account-list.js
+++ frontend/src/components/account/account-list.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import "./account.scss"
 
 const AccountList = (props) => {
     return (
@@ -14,13 +15,44 @@ const AccountList = (props) => {
                 </div>
                 :
                 <div>
-                    {props.accounts.map(account => (
-                        <div key={account.id}>
-                            <Link to={"/account/" + account.id}>
-                                {account.name}
-                            </Link>
+                    <div className="account-grid-outer">
+                        <div className="account-grid-inner centered heading">
+                            <h5 className="grid-header-text">
+                                ID
+                            </h5>
+                            <h5 className="grid-header-text">
+                                Name
+                            </h5>
+                            <h5 className="grid-header-text">
+                                Type
+                            </h5>
                         </div>
-                    ))}
+                    </div>
+                    <hr />
+                    <div className="account-grid-outer">
+                        <div>
+                            {props.accounts.map(account => (
+                                <div className="account-grid-inner" key={account.id}>
+                                    <Link to={"/account/" + account.id}>
+                                        {account.id}
+                                    </Link>
+                                    <hr />
+                                </div>
+                            ))}
+                        </div>
+                        <div>
+                            {props.accounts.map(account => (
+                                <div>
+                                    <div className="account-grid-right" key={account.id}>
+                                        <h5 className="grid-text centered">
+                                            buttons
+                                        </h5>
+                                    </div>
+                                    <hr />
+                                </div>
+                            ))}
+                        </div>
+                    </div>
                 </div>
             }
         </div>
