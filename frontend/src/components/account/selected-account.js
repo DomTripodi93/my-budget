bc7020c33514966c1f9cceb8bc7f9032fefe8bea
DomTripodi93 <dtripodi93@gmail.com>
2020-05-23 09:12:13 -0400

    reformatted detailed view of account

--- frontend/src/components/account/selected-account.js
+++ frontend/src/components/account/selected-account.js
@@ -6,19 +6,15 @@ import { Link } from 'react-router-dom';
 const SelectedAccount = (props) => {
     const account = props.account;
     return (
-        <div className="centered">
-            <Link
-                to={"/account/" + account.name}
-            >
-                <h5 className="grid-text">
-                    selected: {account.name}
-                </h5>
-            </Link>
+        <div className="centered border">
             <h5 className="grid-text">
-                {account.accountType}
+                Account: {account.name}
             </h5>
             <h5 className="grid-text">
-                {account.balance}
+                Type: {account.accountType}
+            </h5>
+            <h5 className="grid-text">
+                Balance: {account.balance}
             </h5>
         </div>
     )
