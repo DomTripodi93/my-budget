89f4b52fb55fc0199dc5ba0784c139c960546a0f
DomTripodi93 <dtripodi93@gmail.com>
2020-05-25 07:52:18 -0400

    added space for buttons on account detail view

--- frontend/src/components/account/selected-account.js
+++ frontend/src/components/account/selected-account.js
@@ -4,27 +4,31 @@ import React from 'react';
 
 const SelectedAccount = (props) => {
     const account = props.account;
-    console.log(account)
     return (
-        <div className="centered border">
-            <h5 className="grid-text">
-                Account: {account.name}
-            </h5>
-            <h5 className="grid-text">
-                Type: {account.accountType}
-            </h5>
-            <h5 className="grid-text">
-                Balance: {account.balance}
-            </h5>
-            {account.active ?
+        <div className="border centered split">
+            <div>
                 <h5 className="grid-text">
-                    Active: Yes
+                    Account: {account.name}
                 </h5>
-                :
                 <h5 className="grid-text">
-                    Active: No
+                    Type: {account.accountType}
                 </h5>
-            }
+                <h5 className="grid-text">
+                    Balance: {account.balance}
+                </h5>
+                {account.active ?
+                    <h5 className="grid-text">
+                        Active: Yes
+                    </h5>
+                    :
+                    <h5 className="grid-text">
+                        Active: No
+                    </h5>
+                }
+            </div>
+            <div>
+                buttons
+            </div>
         </div>
     )
 }
