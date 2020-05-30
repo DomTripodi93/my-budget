b25d78454726dc9376dbed5238037f50413af7a9
DomTripodi93 <dtripodi93@gmail.com>
2020-05-29 20:51:57 -0400

    updated styling of recurring transaction list

--- frontend/src/components/recurring-transaction/recurring-transaction-list.js
+++ frontend/src/components/recurring-transaction/recurring-transaction-list.js
@@ -1,9 +1,21 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { Link } from 'react-router-dom';
 import RecurringTransactionLine from './recurring-transaction-line';
+import "./recurring-transaction.scss"
 
 
 const RecurringTransactionList = (props) => {
+    const [detailsShown, setDetailsShown] = useState({})
+    const showDetails = (name) => {
+        let detailHold = {...detailsShown};
+        detailHold[name] = true;
+        setDetailsShown(detailHold);
+    }
+    const hideDetails = (name) => {
+        let detailHold = {...detailsShown};
+        detailHold[name] = false;
+        setDetailsShown(detailHold);
+    }
     return (
         <div>
             {props.single ?
@@ -12,10 +24,64 @@ const RecurringTransactionList = (props) => {
                 </div>
                 :
                 <div>
-                    {props.transactions.map(transaction => (
-                        <div key={transaction.id}>
+                <div className="recurring-transaction-grid-outer">
+                    <div className="recurring-transaction-grid-inner centered heading">
+                        <h5 className="grid-header-text">
+                            Id
+                        </h5>
+                        <h5 className="grid-header-text">
+                            From
+                        </h5>
+                        <h5 className="grid-header-text">
+                            Cost
+                        </h5>
+                        <h5 className="grid-header-text">
+                            Last
+                        </h5>
+                        <h5 className="grid-header-text">
+                            Next
+                        </h5>
+                        <h5 className="grid-header-text">
+                            Frequency
+                        </h5>
+                    </div>
+                </div>
+                <hr />
+                <div>
+                    <div>
+                        {props.transactions.map(recurringTransaction => (
+                            <div key={recurringTransaction.id}>
+                                <div className="recurring-transaction-grid-outer">
+                                    <div>
+                                        {detailsShown[recurringTransaction.id] ?
+                                            <div>
+                                                {recurringTransaction.id}
+                                            </div>
+                                            :
+                                            <div>
+                                                <RecurringTransactionLine
+                                                    recurringTransaction={recurringTransaction}
+                                                />
+                                            </div>
+                                        }
+                                    </div>
+                                    <div className="recurring-transaction-grid-right center">
+                                        {detailsShown[recurringTransaction.id] ?
+                                            null
+                                            :
+                                            null
+                                        }
+                                    </div>
+                                </div>
+                                <hr />
+                            </div>
+                        ))}
+                    </div>
+                </div>
+                    {props.transactions.map(recurringTransaction => (
+                        <div key={recurringTransaction.id}>
                             <RecurringTransactionLine
-                                recurringTransaction={transaction}
+                                recurringTransaction={recurringTransaction}
                             />
                         </div>
                     ))}
