83791e49149ea17750e9fd058f4028a9dee7ed84
DomTripodi93 <dtripodi93@gmail.com>
2020-07-09 12:05:41 -0400

    added dropDownReducer

--- frontend/src/reducers/root-reducer.js
+++ frontend/src/reducers/root-reducer.js
@@ -4,11 +4,13 @@ import userReducer from './user/user.reducer';
 import accountReducer from './account/account.reducer';
 import transactionReducer from './transaction/transaction.reducer';
 import recurringTransactionReducer from './recurring-transaction/recurring-transaction.reducer';
+import { dropDownReducer } from './drop-down/drop-down.reducer';
 
 
 export default combineReducers({
     user: userReducer,
     account: accountReducer,
     transaction: transactionReducer,
-    recurringTransaction: recurringTransactionReducer
+    recurringTransaction: recurringTransactionReducer,
+    dropDown: dropDownReducer
 })
